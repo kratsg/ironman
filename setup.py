99eb1e39942747f6ac1067d407dbf9c919789f63
Giordon Stark <kratsg@gmail.com>
2015-10-28 11:05:37 -0400

    update setups

--- setup.py
+++ setup.py
@@ -1,24 +1,61 @@
 #!/usr/bin/env python
+from __future__ import print_function
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import io, os, sys
+
+import ironman
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read(os.path.join(here, 'README.rst'))#, 'CHANGES.txt')
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
 
 setup(name='ironman',
-      version='0.1',
+      version=ironman.__version__,
+      url='https://github.com/kratsg/ironman',
+      license='MIT',
       description='SoC Firmware for L1Calo',
-      long_description=''.join(open('README.md').readlines()),
+      long_description=long_description,
       author='Giordon Stark',
       author_email='gstark@cern.ch',
       maintainer='Giordon Stark',
       maintainer_email='gstark@cern.ch',
-      license='MIT',
-      url='',
+      tests_require=['pytest'],
+      install_requires=['Twisted>=15.4.0',
+                        'zope.interface==4.1.3'],
+      cmdclass={'test': PyTest},
       packages=find_packages(),
+      test_suite='ironman.test.test_ironman',
       classifiers=[
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Topic :: Utilities',
-        'Operating System :: POSIX :: Linux',
         'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
         'Intended Audience :: Science/Research',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License (GPL)'
-      ])
+      ],
+      extras_require={
+        'testing': ['pytest'],
+      }
+)
