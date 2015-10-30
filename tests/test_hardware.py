ee983a067503d2f0011d5bf4256079ab2ec3210f
Giordon Stark <kratsg@gmail.com>
2015-10-30 13:20:21 -0400

    move tests into separate classes for hardware

--- tests/test_hardware.py
+++ tests/test_hardware.py
@@ -2,26 +2,28 @@ from zope.interface.verify import verifyClass, verifyObject
 from ironman.hardware import HardwareManager, HardwareMap
 from ironman.interfaces import IHardwareManager, IHardwareMap
 
-def test_hardware_manager_create():
-    obj = HardwareManager()
-    assert obj is not None
+class TestHardwareManager:
+    def test_hardware_manager_create():
+        obj = HardwareManager()
+        assert obj is not None
 
-def test_hardware_manager_class_iface():
-    # Assure the class implements the declared interface
-    assert verifyClass(IHardwareManager, HardwareManager)
+    def test_hardware_manager_class_iface():
+        # Assure the class implements the declared interface
+        assert verifyClass(IHardwareManager, HardwareManager)
 
-def test_hardware_manager_instance_iface():
-    # Assure instances of the class provide the declared interface
-    assert verifyObject(IHardwareManager, HardwareManager())
+    def test_hardware_manager_instance_iface():
+        # Assure instances of the class provide the declared interface
+        assert verifyObject(IHardwareManager, HardwareManager())
 
-def test_hardware_map_create():
-    obj = HardwareMap()
-    assert obj is not None
+class TestHardwareMap:
+    def test_hardware_map_create():
+        obj = HardwareMap()
+        assert obj is not None
 
-def test_hardware_map_class_iface():
-    # Assure the class implements the declared interface
-    assert verifyClass(IHardwareMap, HardwareMap)
+    def test_hardware_map_class_iface():
+        # Assure the class implements the declared interface
+        assert verifyClass(IHardwareMap, HardwareMap)
 
-def test_hardware_map_instance_iface():
-    # Assure instances of the class provide the declared interface
-    assert verifyObject(IHardwareMap, HardwareMap())
+    def test_hardware_map_instance_iface():
+        # Assure instances of the class provide the declared interface
+        assert verifyObject(IHardwareMap, HardwareMap())
