ef12d02faf961f5621166803e9acec96d5bda7c3
Giordon Stark <kratsg@gmail.com>
2015-10-30 13:48:09 -0400

    didn't test right, now fixed by passing in self

--- tests/test_hardware.py
+++ tests/test_hardware.py
@@ -3,27 +3,27 @@ from ironman.hardware import HardwareManager, HardwareMap
 from ironman.interfaces import IHardwareManager, IHardwareMap
 
 class TestHardwareManager:
-    def test_hardware_manager_create():
+    def test_hardware_manager_create(self):
         obj = HardwareManager()
         assert obj is not None
 
-    def test_hardware_manager_class_iface():
+    def test_hardware_manager_class_iface(self):
         # Assure the class implements the declared interface
         assert verifyClass(IHardwareManager, HardwareManager)
 
-    def test_hardware_manager_instance_iface():
+    def test_hardware_manager_instance_iface(self):
         # Assure instances of the class provide the declared interface
         assert verifyObject(IHardwareManager, HardwareManager())
 
 class TestHardwareMap:
-    def test_hardware_map_create():
+    def test_hardware_map_create(self):
         obj = HardwareMap()
         assert obj is not None
 
-    def test_hardware_map_class_iface():
+    def test_hardware_map_class_iface(self):
         # Assure the class implements the declared interface
         assert verifyClass(IHardwareMap, HardwareMap)
 
-    def test_hardware_map_instance_iface():
+    def test_hardware_map_instance_iface(self):
         # Assure instances of the class provide the declared interface
         assert verifyObject(IHardwareMap, HardwareMap())
