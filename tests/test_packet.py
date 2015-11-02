2c206a6bc0055999caededcf87f345c1e2ace42e
Giordon Stark <kratsg@gmail.com>
2015-11-02 17:42:24 -0500

    comments for new tests to write for checking shit

--- tests/test_packet.py
+++ tests/test_packet.py
@@ -13,3 +13,10 @@ def test_ipbus_packet_class_iface():
 def test_ipbus_packet_instance_iface():
     # Assure instances of the class provide the declared interface
     assert verifyObject(IIPBusPacket, IPBusPacket())
+
+# test packets using the following:
+# little endian packet
+#data = '\xf0\x00\x00 \x0f\x01\x00 \x03\x00\x00\x00'
+# big endian packet
+#data = ' \x00\x00\xf0 \x00\x01\x0f\x00\x00\x00\x03'
+
