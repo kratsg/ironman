7b575418f67049175c368a08bb53b8ee51362538
Giordon Stark <kratsg@gmail.com>
2015-11-02 09:48:07 -0500

    mock up example commands

--- tests/test_server.py
+++ tests/test_server.py
@@ -2,6 +2,9 @@ from zope.interface.verify import verifyClass, verifyObject
 from ironman.server import IPBusServerProtocol, IPBusServerFactory
 from twisted.internet.interfaces import IProtocol, IProtocolFactory
 
+# fixtures for passing in the objects
+import pytest
+
 class TestServerProtocol:
     def test_server_protocol_create(self):
         obj = IPBusServerProtocol()
@@ -27,3 +30,28 @@ class TestServerFactory:
     def test_server_factory_instance_iface(self):
         # Assure instances of the class provide the declared interface
         assert verifyObject(IProtocolFactory, IPBusServerFactory())
+
+@pytest.fixture
+def ipbus_server():
+    from twisted.test import proto_helpers
+    factory = IPBusServerFactory()
+    protocol = factory.buildProtocol(('127.0.0.1', 0))
+    transport = proto_helpers.StringTransport()
+    protocol.makeConnection(transport)
+
+class TestIPBus:
+    @pytest.fixture(autouse=True)
+    def init_server(self):
+        from twisted.test import proto_helpers
+        factory = IPBusServerFactory()
+        self.proto = factory.buildProtocol(('127.0.0.1', 0))
+        self.tr = proto_helpers.StringTransport()
+        self.proto.makeConnection(tr)
+
+    @pytest.mark.parametrize("inbound,expected", [
+        ("write", "written"),
+        ("read", "value"),
+    ])
+    def test_command(self, inbound, expected):
+        self.proto.dataReceived(inbound)
+        assert self.tr.value == expected
