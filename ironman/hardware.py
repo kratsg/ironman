402441c67b61f2b6ce72c1c82be2ab5ca4f2f43b
Giordon Stark <kratsg@gmail.com>
2015-11-03 01:28:03 -0500

    pass all interface tests.

--- ironman/hardware.py
+++ ironman/hardware.py
@@ -3,7 +3,21 @@ from ironman.interfaces import IHardwareManager, IHardwareMap
 
 class HardwareManager(object):
     implements(IHardwareManager)
-    pass
+
+    def check_data(self, address, data):
+        pass
+
+    def check_address(self, address):
+        pass
+
+    def find_address(self, address):
+        pass
+
+    def get_checksum(self, map_name):
+        pass
+
+    def add(self, map):
+        pass
 
 class HardwareMap(object):
     implements(IHardwareMap)
