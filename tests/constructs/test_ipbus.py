daf9c83be3f6a48e19daaf8a90adddd1cec36416
Giordon Stark <kratsg@gmail.com>
2015-11-06 12:05:18 -0500

    restructure, test for control packet header

--- tests/constructs/test_ipbus.py
+++ tests/constructs/test_ipbus.py
@@ -1,4 +1,4 @@
-from ironman.constructs.ipbus import IPBusConstruct
+from ironman.constructs.ipbus import IPBusConstruct, PacketHeaderStruct, ControlHeaderStruct
 from ironman.globals import TESTPACKETS
 from construct import FieldError, ValidationError
 
@@ -11,13 +11,22 @@ def test_fail_parsing_little_endian():
     with pytest.raises(ValidationError) as e:
         IPBusConstruct.parse(TESTPACKETS['little-endian'])
 
-@pytest.mark.parametrize("data", [TESTPACKETS['big-endian'][:i] for i in range(4)])
-def test_bad_ipbus_packet_header(data):
-    """ This test just runs over a technically valid, yet incomplete ipbus packet header
-    """
-    with pytest.raises(FieldError) as e:
-        IPBusConstruct.parse(data)
+class TestIPBusPacketHeader:
+    @pytest.mark.parametrize("data", [TESTPACKETS['big-endian'][:i] for i in range(PacketHeaderStruct.sizeof())])
+    def test_bad_ipbus_packet_header(self, data):
+        """ This test just runs over a technically valid, yet incomplete ipbus packet header
+        """
+        with pytest.raises(FieldError) as e:
+            PacketHeaderStruct.parse(data)
 
-def test_bad_ipbus_version():
-    with pytest.raises(ValidationError) as e:
-        p = IPBusConstruct.parse(TESTPACKETS['wrong protocol version'])
+    def test_bad_ipbus_version(self):
+        with pytest.raises(ValidationError) as e:
+            PacketHeaderStruct.parse(TESTPACKETS['wrong protocol version'])
+
+class TestIPBusControlPacket:
+    @pytest.mark.parametrize("data", [TESTPACKETS['big-endian'][PacketHeaderStruct.sizeof():i] for i in range(PacketHeaderStruct.sizeof(), ControlHeaderStruct.sizeof()+4)])
+    def test_bad_control_packet_header(self, data):
+        """ This test just runs over a technically valid, yet incomplete control transaction header
+        """
+        with pytest.raises(FieldError) as e:
+            ControlHeaderStruct.parse(data)
