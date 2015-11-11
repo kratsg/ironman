2ce63b610ba38f28694408a09e153cd0bbc78d1e
Giordon Stark <kratsg@gmail.com>
2015-11-10 22:35:27 -0500

    Use type.__subclasses__() instead of metaclassing for registering next...

--- ironman/communicator.py
+++ ironman/communicator.py
@@ -51,3 +51,20 @@ class Jarvis(object):
             return protocol.read(transaction.address, transaction.words)
         elif transaction.type_id == 'WRITE':
             return protocol.write(transaction.address, transaction.data)
+
+class SimpleIO(Jarvis):
+    __route__ = 'file'
+    __f__ = None
+    implements(ICommunicationProtocol)
+
+    def read(self, offset, size):
+        with open(self.__f__, 'rb') as f:
+            f.seek(offset)
+            return f.read(size)
+
+    def write(self, offset, data):
+        with open(self.__f__, 'r+b') as f:
+            f.seek(offset)
+            return f.write(data)
+
+
