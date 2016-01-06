6ee79fe1628e503c9d82b75b2efa0e9aa6d3a35a
Giordon Stark <kratsg@gmail.com>
2016-01-06 17:58:06 -0500

    Add more tests for history

--- ironman/history.py
+++ ironman/history.py
@@ -12,8 +12,11 @@ class History(dict):
         self.packets = collections.deque([None]*self.maxlen, maxlen=self.maxlen)
 
     def record(self, packet):
+        # make sure packet doesn't exist first
+        if packet in self.packets:
+            raise ValueError("This packet has already been recorded in the history.")
         # Attempt to delete from history only if we are removing from queue
-        if len(self.packets) >= self.maxlen:
+        if self.packets[0] is not None:
             del self[self.packets[0].request.header.id]
         # Add new packet to history
         self.packets.append(packet)
--- tests/test_history.py
+++ tests/test_history.py
@@ -4,6 +4,7 @@ from ironman.interfaces import IHistory
 from ironman.utilities import byteswap
 
 from ironman.globals import IPBUS_VERSION, TESTPACKETS
+from ironman.packet import IPBusPacket
 
 # fixtures for passing in the objects
 import pytest
@@ -19,3 +20,21 @@ def test_history_class_iface():
 def test_history_instance_iface():
     # Assure instances of the class provide the declared interface
     assert verifyObject(IHistory, History())
+
+def test_history_empty():
+    h = History()
+    assert len(h) == 0
+    assert any(h.packets) == False
+    assert len(h.packets) == h.maxlen
+
+def test_history_record():
+    h = History()
+    for i in range(101):
+        p = IPBusPacket(TESTPACKETS['big-endian'])
+        p.request.header.id = i
+        h.record(p)
+    assert len(h) == h.maxlen
+    assert all(h.packets) == True
+    assert 0 not in h
+    assert 1 in h
+    assert 100 in h
