609e4c42b405a609b9e0d0ca6f52ef4fd331bf67
Giordon Stark <kratsg@gmail.com>
2018-03-23 11:08:43 -0500

    update ipbus

--- ironman/constructs/ipbus.py
+++ ironman/constructs/ipbus.py
@@ -1,28 +1,27 @@
-from construct import Array, BitField, BitStruct, Embed, Enum, GreedyRange, Struct, Switch, UBInt32, SBInt32, OneOf, Pass
-from ..globals import IPBUS_VERSION
+from construct import Array, BitsInteger, BitStruct, Enum, GreedyRange, Struct, Int8ub, Int32ub, Int32ul, Int32sb, Int32sl, OneOf, Nibble, Octet, If, IfThenElse, ByteSwapped, this, Computed, Switch, Pointer
+from ironman.globals import IPBUS_VERSION
 
-from ironman.utilities import PrintContext
-
-PacketHeaderStruct = BitStruct("header",
-                        OneOf(BitField("protocol_version", 4), [IPBUS_VERSION]),
-                        OneOf(BitField("reserved", 4), [0x0]),
-                        BitField("id", 16),
-                        OneOf(BitField("byteorder", 4), [0xf]),
-                        Enum(BitField("type_id", 4),
-                            CONTROL = 0x0,
-                            STATUS = 0x1,
-                            RESEND = 0x2
-                        )
+PacketHeaderStruct = BitStruct(
+                        "protocol_version" / OneOf(Nibble, [IPBUS_VERSION]),
+                        "reserved" / OneOf(Nibble, [0x0]),
+                        "id" / BitsInteger(16),
+                        "byteorder" / OneOf(Nibble, [0xf]),
+                        "type_id" / Enum(Nibble,
+                          CONTROL = 0x0,
+                          STATUS = 0x1,
+                          RESEND = 0x2)
 )
 """
 Struct detailing the Packet Header logic
+
+byteorder is `0xf` if big-endian and `0x0` if little-endian
 """
 
-ControlHeaderStruct = BitStruct("transaction",
-                        OneOf(BitField("protocol_version", 4), [IPBUS_VERSION]),
-                        BitField("id", 12),
-                        BitField("words", 8),
-                        Enum(BitField("type_id", 4),
+ControlHeaderStruct = BitStruct(
+                         "protocol_version" / OneOf(Nibble, [IPBUS_VERSION]),
+                         "id" / BitsInteger(12),
+                         "words" / Octet,
+                         "type_id" / Enum(Nibble,
                             READ = 0x0,
                             NOINCREAD = 0x2,
                             WRITE = 0x1,
@@ -32,7 +31,7 @@ ControlHeaderStruct = BitStruct("transaction",
                             RCONFIG = 0x6,
                             WCONFIG = 0x7
                         ),
-                        Enum(BitField("info_code", 4),
+                        "info_code" / Enum(Nibble,
                             SUCCESS = 0x0,
                             BADHEADER = 0x1,
                             RBUSERROR = 0x4,
@@ -46,106 +45,57 @@ ControlHeaderStruct = BitStruct("transaction",
 Struct detailing the Control Header logic
 """
 
-ReadStruct = Struct("read",
-                Switch("data", lambda ctx: ctx.info_code,
-                    {
-                        "REQUEST": Pass,
-                        "SUCCESS": Array(lambda ctx: ctx.words, UBInt32("data"))
-                    },
-                    default = Pass
-                )
+ControlStruct = "ControlTransaction" / Struct(
+                    "header" / IfThenElse(this._.bigendian, ControlHeaderStruct, ByteSwapped(ControlHeaderStruct)),
+                    "address" / IfThenElse(this._.bigendian, Int32ub, Int32ul),
+										"data" / Switch(lambda ctx: (ctx.header.type_id, ctx.header.info_code), {
+											("READ","SUCCESS"): Array(this.header.words, IfThenElse(this._.bigendian, Int32ub, Int32ul)),
+											("NOINCREAD","SUCCESS"): Array(this.header.words, IfThenElse(this._.bigendian, Int32ub, Int32ul)),
+											("RCONFIG","SUCCESS"): Array(this.header.words, IfThenElse(this._.bigendian, Int32ub, Int32ul)),
+                      ("WRITE","REQUEST"): Array(this.header.words, IfThenElse(this._.bigendian, Int32ub, Int32ul)),
+                      ("NOINCWRITE","REQUEST"): Array(this.header.words, IfThenElse(this._.bigendian, Int32ub, Int32ul)),
+                      ("WCONFIG","REQUEST"): Array(this.header.words, IfThenElse(this._.bigendian, Int32ub, Int32ul)),
+                      ("RMWBITS", "REQUEST"): ["and" / IfThenElse(this._.bigendian, Int32ub, Int32ul), "or" / IfThenElse(this._.bigendian, Int32ub, Int32ul)],
+                      ("RMWBITS", "SUCCESS"): IfThenElse(this._.bigendian, Int32ub, Int32ul),
+                      ("RMWSUM", "REQUEST"): IfThenElse(this._.bigendian, Int32sb, Int32sl),  # note: signed 32-bit for subtraction!
+                      ("RMWSUM", "SUCCESS"): IfThenElse(this._.bigendian, Int32ub, Int32ul)
+										})
 )
-"""
-Struct detailing the Read Transaction logic
-"""
 
-WriteStruct = Struct("write",
-                Switch("data", lambda ctx: ctx.info_code,
-                    {
-                        "REQUEST": Array(lambda ctx: ctx.words, UBInt32("data")),
-                        "SUCCESS": Pass
-                    },
-                    default = Pass
-                )
-)
-"""
-Struct detailing the Write Transaction logic
-"""
-
-RMWBitsStruct = Struct("rmwbits",
-                Switch("data", lambda ctx: ctx.info_code,
-                    {
-                        "REQUEST": Embed(Struct("data", UBInt32("and"), UBInt32("or"))),
-                        "SUCCESS": UBInt32("data")
-                    },
-                    default = Pass
-                )
-)
-"""
-Struct detailing the RMWBits Transaction logic
-
-Should compute via :math:`X \Leftarrow (X\wedge A)\\vee (B\wedge(!A))`
-"""
-
-RMWSumStruct = Struct("rmwsum",
-                Switch("data", lambda ctx: ctx.info_code,
-                    {
-                        "REQUEST": SBInt32("addend"),  # note: signed 32-bit for subtraction!
-                        "SUCCESS": UBInt32("data")
-                    },
-                    default = Pass
-                )
-)
-"""
-Struct detailing the RMWSum Transaction logic
-
-Should compute via :math:`X \Leftarrow X+A`
-"""
-
-ControlStruct = Struct("ControlTransaction",
-                    Embed(ControlHeaderStruct),
-                    UBInt32("address"),
-                    Embed(Switch("data", lambda ctx: ctx.type_id,
-                        {
-                            "READ": ReadStruct,
-                            "NOINCREAD": ReadStruct,
-                            "WRITE": WriteStruct,
-                            "NOINCWRITE": WriteStruct,
-                            "RMWBITS": RMWBitsStruct,
-                            "RMWSUM": RMWSumStruct,
-                            "RCONFIG": ReadStruct,
-                            "WCONFIG": WriteStruct
-                        }
-                    ))
-)
 """
 Struct detailing the Control Action logic
+
+.. note::
+
+  - RMWBits: Should compute via :math:`X \Leftarrow (X\wedge A)\\vee (B\wedge(!A))`
+  - RMWSum: Should compute via :math:`X \Leftarrow X+A`
+
 """
 
-StatusRequestStruct = Struct("StatusTransaction", Array(15, OneOf(UBInt32("data"), [0])))
-StatusResponseStruct = Struct("StatusTransaction", Array(15, UBInt32("data")))
+StatusRequestStruct = "StatusTransaction" / Struct("data" / Array(15, OneOf(Int32ub, [0])))
+StatusResponseStruct = "StatusTransaction" / Struct("data" / Array(15, Int32ub))
 """
 Struct detailing the Status Action logic
 """
 
-ResendStruct = Struct("ResendTransaction")
+ResendStruct = "ResendTransaction" / Struct()
 """
 Struct detailing the Resend Action logic
 """
 
-IPBusWords = Struct('IPBusWords', GreedyRange(UBInt32('data')))
+IPBusWords = "IPBusWords" / Struct("data" / GreedyRange(Int32ub))
 
-IPBusConstruct = Struct("IPBusPacket",
-                    PacketHeaderStruct,  # defined as 'header' in context
-                    Switch("data", lambda ctx: ctx.header.type_id,
-                        {
-                            "CONTROL" : GreedyRange(ControlStruct),
-                            "STATUS" : StatusRequestStruct,
-                            "RESEND" : ResendStruct,
-                        }
-                    )
+IPBusConstruct = "IPBusPacket" / Struct(
+                    "pointer" / Pointer(3, Int8ub),
+                    "bigendian" / Computed(this.pointer == 0xf0),
+                    "header" / IfThenElse(this.bigendian, PacketHeaderStruct, ByteSwapped(PacketHeaderStruct)),  # defined as 'header' in context
+
+                    "transactions" / If(lambda ctx: ctx.header.type_id == "CONTROL", GreedyRange(ControlStruct)),
+                    "status" / If(lambda ctx: ctx.header.type_id == "STATUS", StatusRequestStruct),
+                    "resend" / If(lambda ctx: ctx.header.type_id == "RESEND", ResendStruct),
 )
 """
 Top-level IPBus Construct which is a packet parser/builder
 """
+
 
