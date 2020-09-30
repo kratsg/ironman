8f58254e268ea20b344de5c932994c3c995ed74d
Emily Smith <easmith5@uchicago.edu>
2020-09-30 15:09:29 +0200

    Add FauxCP for L1Calo's TCP which isn't really TCP

--- ironman/server.py
+++ ironman/server.py
@@ -1,6 +1,7 @@
 # Server Class managing requests over udp
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet.protocol import ServerFactory, Protocol, DatagramProtocol
+import struct
 
 class UDP(DatagramProtocol):
     def __init__(self, dgen):
@@ -42,11 +43,43 @@ class TCPFactory(ServerFactory):
     def buildProtocol(self, addr):
         return self.protocol(self.d)
 
+class FauxCP(Protocol):
+    def __init__(self, dgen):
+        self.d = dgen
+
+    def _stripPreIPBusHeader(self, data):
+        return data[4:]
+
+    def _addPreIPBusHeader(self,data):
+        return struct.pack(">I", len(data)) + data
+        
+    def dataReceived(self, data):
+        """
+        After receiving the data, generate the deferreds and add myself to it.
+        """
+        def write(result):
+            fauxResult = self._addPreIPBusHeader(result)
+            print "Writing %r" % fauxResult
+            self.transport.write(fauxResult)
+        d = self.d()
+        d.addCallback(write)  # errors are silently ignored!
+        d.callback(self._stripPreIPBusHeader(data))
+
+class FauxFactory(ServerFactory):
+    protocol = FauxCP
+    def __init__(self, dgen):
+        self.d = dgen
+
+    def buildProtocol(self, addr):
+        return self.protocol(self.d)
+
 
 def ServerFactory(proto, dgen):
     if proto == 'udp':
         return UDP(dgen)
     elif proto == 'tcp':
         return TCPFactory(dgen)
+    elif proto == 'fauxcp':
+        return FauxFactory(dgen)
     else:
         return None
